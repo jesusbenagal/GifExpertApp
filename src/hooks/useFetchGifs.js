3fff9da54f93f5be31f0429db076fbbd2e6f1950
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-04-12 19:50:20 +0200

    Añadiendo animated.css a los componentes

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -15,15 +15,11 @@ export const useFetchGifs = ( category ) => {
         getGifs( category )
             .then ( imgs => {
 
-                setTimeout( () => {
-
                     setState ({
                         data: imgs,
                         loading: false
                     });
-
-                }, 3000);
-
+                    
             })
 
     }, [ category ]);
