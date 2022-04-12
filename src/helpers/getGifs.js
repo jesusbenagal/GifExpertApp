34cf94c4087920d8169528746b5e9423e97311b1
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-04-12 20:19:27 +0200

    Añadiendo Key Giphy

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,9 +1,7 @@
 
 export const getGifs = async( category ) => {
 
-    const apiKey = process.env.REACT_APP_API_KEY_GIPHY
-
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=${ apiKey }`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=l4n7eAhGRXGTbvFk0lT5MzisI70lYr7n`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
